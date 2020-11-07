dfe93e492170a858eed7d321d5ae637162e6f473
Harshit Gupta <kohthecodemaster@gmail.com>
2020-11-07 20:54:24 +0530

    30th Commit - Day 19 - DOM Manipulation 1. Add, remove & toggle classList 2. textContent & innerHTML 3. getAttribute & setAttribute 4. Iterating node list to manipulate altogether rather than selecting elements explicitly
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- units/dom/dom-manipulation/script.js
+++ units/dom/dom-manipulation/script.js
@@ -2,68 +2,38 @@
 //  Wait for the html content to be loaded before running the major function
 document.addEventListener("DOMContentLoaded", () => {
 
-    domSelect();
-
     domManipulate();
 
 });
 
-function domSelect() {
-
-    //  Get element by id
-    console.log("\ndocument.getElementById(elementId)");
-    let item = document.getElementById("exclusive");
-    console.log(item);
-    console.log("Text Content: " + item.textContent);
-
-    /*
-        Get node list of elements by class name
-        Node list is similar to array but actually it is an object
-        node list doesn't have forEach method unlike arrays
-    */
-    console.log("\ndocument.getElementsByClassName(className)");
-    let itemsNodeList = document.getElementsByClassName("heading");
-    console.log(itemsNodeList);
-    console.log(itemsNodeList[0]);
-
-    //  Get node list of elements by tag name
-    console.log("\ndocument.getElementsByTagName(tagName)");
-    itemsNodeList = document.getElementsByTagName("h1");
-    console.log(itemsNodeList);
-    console.log(itemsNodeList[1]);
-
-    //  Get the first item by css query selector
-    console.log("\ndocument.querySelector(cssSelector)");
-    console.log(document.querySelector("#exclusive"));
-    console.log(document.querySelector(".heading"));
-    console.log(document.querySelector("h1"));
-
-    //  Get node list of elements by css query selector
-    console.log("\ndocument.querySelectorAll(cssSelector)");
-    itemsNodeList = document.querySelectorAll("h1");
-    console.log(itemsNodeList);
-
-}
-
 function domManipulate() {
 
     //  Find body element by tag name
-    let body = document.getElementsByTagName("body")[0];
-    let hasChanged = false;
+    let headings = document.getElementsByTagName("h1");
 
-    //  Update text color & background color of body every 1000 ms
-    setInterval(function () {
+    //  Iterate headings node list and add heading class to each h1 tag
+    for (let i = 0; i < headings.length; i++) {
+        headings[i].classList.add("heading");
+    }
 
-        if (hasChanged) {
-            body.style.backgroundColor = "grey";
-            body.style.color = "black";
-        } else {
-            body.style.backgroundColor = "black";
-            body.style.color = "white";
-        }
-        hasChanged = !hasChanged;
+    //  Toggle heading class for 2nd h1 tag
+    headings[1].classList.toggle("heading");
 
-    }, 1000);
+    let secondLink = document.getElementsByTagName("a")[1];
+
+    //  Set attribute for element by providing the attribute name followed by the value for that attribute
+    secondLink.setAttribute("href", "https://facebook.com");
+    secondLink.setAttribute("name", "Facebook");
+    secondLink.textContent = "Link To " + secondLink.getAttribute("name") + ".com";
+
+    //  Reset textContent for ol element with id #test-textcontent
+    //  Updating textContent overwrites all the nested html tags
+    document.querySelector("#test-textcontent").textContent = "Ordered List has been removed.";
+
+    //  Reset innerHTML for div element with id #test-innerhtml
+    //  Updating innerHTML overwrites previous html tags but retains the html tags of new value
+    document.querySelector("#test-innerhtml").innerHTML = "<p>Lorem ipsum dolor sit amet," +
+        " consectetur adipisicing elit. Odio, officiis!</p>";
 
 }
 
