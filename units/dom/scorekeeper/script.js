f769c7ba8705cc78b618fc8a134cfaba7de61f75
Harshit Gupta <kohthecodemaster@gmail.com>
2020-11-07 22:22:02 +0530

    32nd Commit - Day 19 - Event Listeners for Scorekeeper (Advanced DOM Manipulation) 1. Event Listeners for elements
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- units/dom/scorekeeper/script.js
+++ units/dom/scorekeeper/script.js
@@ -1,44 +1,87 @@
-//  document is the root element of all the objects model for every html page
+//  Define element variables
+let scoreboard;
+let wrapperBestOfN;
+let inputBestOfN;
+let btnP1, btnP2, btnReset;
+
+//  Initialize default values
+let bestOfN = 5;
+let p1 = 0;
+let p2 = 0;
+
 //  Wait for the html content to be loaded before running the major function
 document.addEventListener("DOMContentLoaded", () => {
 
-    // domManipulate();
+    major();
 
 });
 
-function domManipulate() {
+function major() {
 
-    //  Find body element by tag name
-    let headings = document.getElementsByTagName("h1");
-
-    //  Iterate headings node list and add heading class to each h1 tag
-    for (let i = 0; i < headings.length; i++) {
-        headings[i].classList.add("heading");
-    }
-
-    //  Toggle heading class for 2nd h1 tag
-    headings[1].classList.toggle("heading");
-
-    let secondLink = document.getElementsByTagName("a")[1];
-
-    //  Set attribute for element by providing the attribute name followed by the value for that attribute
-    secondLink.setAttribute("href", "https://facebook.com");
-    secondLink.setAttribute("name", "Facebook");
-    secondLink.textContent = "Link To " + secondLink.getAttribute("name") + ".com";
-
-    //  Reset textContent for ol element with id #test-textcontent
-    //  Updating textContent overwrites all the nested html tags
-    document.querySelector("#test-textcontent").textContent = "Ordered List has been removed.";
-
-    //  Reset innerHTML for div element with id #test-innerhtml
-    //  Updating innerHTML overwrites previous html tags but retains the html tags of new value
-    document.querySelector("#test-innerhtml").innerHTML = "<p>Lorem ipsum dolor sit amet," +
-        " consectetur adipisicing elit. Odio, officiis!</p>";
+    init();
 
 }
 
+function init() {
 
+    //  Initialize elements
+    scoreboard = document.querySelector("#scoreboard");
+    wrapperBestOfN = document.querySelector("#best-of-n");
+    inputBestOfN = document.querySelector("#input-best-of-n");
+    btnP1 = document.querySelector("#btn-player-one");
+    btnP2 = document.querySelector("#btn-player-two");
+    btnReset = document.querySelector("#btn-reset");
 
+    //  Apply Event Listeners
+    btnP1.addEventListener("click", updateP1Score);
+    btnP2.addEventListener("click", updateP2Score);
+    inputBestOfN.addEventListener("change", updateBestOfN);
+    btnReset.addEventListener("click", resetStats);
 
+}
 
+function resetStats() {
+    scoreboard.textContent = "0 - 0";
+    p1 = 0;
+    p2 = 0;
+}
+
+function updateBestOfN() {
+
+    if (checkGameOver()) {
+        inputBestOfN.value = bestOfN;
+        return;
+    }
+    let num = inputBestOfN.value;
+    bestOfN = num > 10 ? 10 : num < 1 ? 1 : num;
+    this.value = bestOfN;
+    wrapperBestOfN.textContent = "Best Of " + bestOfN;
+
+}
+
+function updateP1Score() {
+    p1 += 1;
+    if (checkGameOver()) return;
+    scoreboard.textContent = p1 + " - " + p2;
+}
+
+function updateP2Score() {
+    p2 += 1;
+    if (checkGameOver()) return;
+    scoreboard.textContent = p1 + " - " + p2;
+}
+
+function checkGameOver() {
+
+    if (p1 >= bestOfN) {
+        scoreboard.textContent = "Player One WON!";
+        return true;
+    }
+    if (p2 >= bestOfN) {
+        scoreboard.textContent = "Player Two WON!";
+        return true;
+    }
+
+    return false;
+}
 
