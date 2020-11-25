37c8d411ca7584b5552c45bce90ee0eee62affba
Harshit Gupta <kohthecodemaster@gmail.com>
2020-11-25 20:45:13 +0530

    39th Commit - Day 37 - Post Requests 1. Using app.post for post request 2. html form for making post requests 3. Using body-parser library to parse http request body into javascript object
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- backend/EJSDemoApp/app.js
+++ backend/EJSDemoApp/app.js
@@ -1,8 +1,12 @@
 let express = require("express");
+let bodyparser = require("body-parser");
 let app = express();
 
+app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+let itemList = ["one", 2, 3.14, true];
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -10,6 +14,15 @@ app.get("/", function (req, res) {
 
 app.get("/test", function (req, res) {
     res.render("test");
+});
+
+app.get("/showlist", function (req, res) {
+    res.render("showlist", {itemList: itemList});
+});
+
+app.post("/addNewItem", function (req, res) {
+    itemList.push(req.body.newItem);
+    res.redirect("/showlist");
 });
 
 app.listen(3000, function () {
