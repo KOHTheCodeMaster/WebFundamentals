7f61239dee7cd59c79591c18f2a8b42785de168e
Harshit Gupta <kohthecodemaster@gmail.com>
2020-12-18 22:59:13 +0530

    46th Commit - Day 60 - Simple-Auth-App - Configure Passport & Register User
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- backend/authentication/simple-auth-app/index.js
+++ backend/authentication/simple-auth-app/index.js
@@ -1,12 +1,27 @@
 let express = require("express"),
     mongoose = require("mongoose"),
-    bodyparser = require("body-parser"),
+    bodyParser = require("body-parser"),
+    passport = require("passport"),
+    // LocalStrategy = require("passport-local"),
+    // passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user"),
     app = express();
 
 //  App Configuration
 //  --------------------------------------------------
 
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "This is a secret key used for encryption/decryption of messages.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -25,22 +40,36 @@ mongoose.connect("mongodb://localhost:27017/Simple-Auth-App-V1", function (err) 
 
 //  Root page route
 app.get("/", function (req, res) {
-    res.redirect("/login");
-});
-
-// Login index page route
-app.get("/login", function (req, res) {
-    res.render("login");
-});
-
-// Login post page route
-app.post("/login", function (req, res) {
-    res.render("home", {username: req.body.username});
+    res.redirect("/register");
 });
 
 // Register index page route
 app.get("/register", function (req, res) {
     res.render("register");
+});
+
+// Register post page route
+app.post("/register", function (req, res) {
+
+    // Register username in db along with hash of the password rather than raw string
+    User.register(new User({username: req.body.username}), req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        //  Authenticate user & render home page
+        //  Doubt: Strange method parameter syntax
+        passport.authenticate("local")(req, res, function () {
+            res.render("home", {user: user});
+        });
+    });
+
+});
+
+// Login index page route
+app.get("/login", function (req, res) {
+    res.render("login");
 });
 
 
